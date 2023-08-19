874c0d423003cefd4e1f49bd74b4209b9a39d63e
philmarkm <91298947+philmarkm@users.noreply.github.com>
2023-08-19 16:14:15 -0500

    Add files via upload

---
+++ slideshow.js
@@ -0,0 +1,23 @@
+let slideIndex = 0;
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("event-image");
+
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+
+  slideIndex++;
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
+
+  slides[slideIndex - 1].style.display = "block";
+
+  // Change slide every 5 seconds (5000 milliseconds)
+  setTimeout(showSlides, 5000);
+}
+
+// Call the function to show the first slide
+showSlides();
