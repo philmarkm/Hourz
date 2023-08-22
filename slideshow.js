17e0dc5756dc633eb11a9b835fc4992fd0b48d72
philmarkm <91298947+philmarkm@users.noreply.github.com>
2023-08-22 10:07:21 -0500

    Add files via upload

--- slideshow.js
+++ slideshow.js
@@ -1,33 +1,16 @@
-let slideIndex = 0;
-
-function showSlides() {
-  let i;
-  let slides = document.getElementsByClassName("event-image");
-
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-
-  slideIndex++;
-  if (slideIndex > slides.length) {
-    slideIndex = 1;
-  }
-
-  slides[slideIndex - 1].style.display = "block";
-
-  // Change slide every 5 seconds (5000 milliseconds)
-  setTimeout(showSlides, 5000);
-}
-
-// Call the function to show the first slide
-showSlides();
-
 const introVideo = document.getElementById("intro-video");
     const content = document.getElementById("content");
 
     // When the video loads and starts playing
     introVideo.addEventListener("canplaythrough", () => {
       introVideo.play();
+    });
+
+    // Video playback error
+    introVideo.addEventListener("error", () => {
+      introVideo.style.display = "none";
+      content.classList.remove("hidden");
+      document.body.style.overflow = "visible"; // Allow scrolling
     });
 
     // When the video ends, hide the video and show the rest of the content
@@ -39,4 +22,28 @@ const introVideo = document.getElementById("intro-video");
       document.body.style.overflow = "visible"; // Allow scrolling
     });
 
+    // Rest of your code
+    let slideIndex = 0;
+
+    function showSlides() {
+      let i;
+      let slides = document.getElementsByClassName("event-image");
+
+      for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+      }
+
+      slideIndex++;
+      if (slideIndex > slides.length) {
+        slideIndex = 1;
+      }
+
+      slides[slideIndex - 1].style.display = "block";
+
+      // Change slide every 5 seconds (5000 milliseconds)
+      setTimeout(showSlides, 5000);
+    }
+
+    // Call the function to show the first slide
+    showSlides();
 
