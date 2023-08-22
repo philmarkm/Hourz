8882c32ec49b0009ff08b0594011a6d8371bc3cf
philmarkm <91298947+philmarkm@users.noreply.github.com>
2023-08-22 16:06:17 -0500

    Add files via upload

--- slideshow.js
+++ slideshow.js
@@ -25,5 +25,26 @@ showSlides();
 const introVideo = document.getElementById("intro-video");
 const content = document.getElementById("content");
 
+const eventDate = new Date(2023, 9, 1, 0, 0, 0); // October 01, 2023, 12:00 AM
 
+function updateCountdown() {
+  const now = new Date();
+  const timeDifference = eventDate - now;
+
+  if (timeDifference <= 0) {
+    document.getElementById('countdown').innerHTML = 'Event has started!';
+  } else {
+    const days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((timeDifference % (1000 * 60)) / 1000);
+
+    document.getElementById('countdown').innerHTML = `
+      ${days}d ${hours}h ${minutes}m ${seconds}s
+    `;
+  }
+}
+
+// Call the updateCountdown function to start the countdown and update it every second
+setInterval(updateCountdown, 1000);
 
