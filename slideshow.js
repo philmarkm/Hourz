cd3b618415356bea71520af353bdfeb2bce2c500
philmarkm <91298947+philmarkm@users.noreply.github.com>
2023-08-22 10:19:13 -0500

    Add files via upload

--- slideshow.js
+++ slideshow.js
@@ -1,49 +1,49 @@
+const isiPhone = /iPhone/i.test(navigator.userAgent);
 const introVideo = document.getElementById("intro-video");
-    const content = document.getElementById("content");
+const introVideoWrapper = document.getElementById("intro-video-wrapper");
+const content = document.getElementById("content");
 
-    // When the video loads and starts playing
-    introVideo.addEventListener("canplaythrough", () => {
-      introVideo.play();
-    });
+if (isiPhone) {
+  introVideoWrapper.style.display = "none"; // Hide the video for iPhone users
+  content.style.display = "block"; // Show the content
+} else {
+  // When the video loads and starts playing
+  introVideo.addEventListener("canplaythrough", () => {
+    introVideo.play();
+  });
 
-    // Video playback error
-    introVideo.addEventListener("error", () => {
-      introVideo.style.display = "none";
-      content.classList.remove("hidden");
-      document.body.style.overflow = "visible"; // Allow scrolling
-    });
+  // When the video ends, hide the video and show the rest of the content
+  introVideo.addEventListener("ended", () => {
+    introVideo.pause();
+    introVideo.currentTime = 0;
+    introVideoWrapper.style.display = "none";
+    content.style.display = "block";
+    document.body.style.overflow = "visible"; // Allow scrolling
+  });
 
-    // When the video ends, hide the video and show the rest of the content
-    introVideo.addEventListener("ended", () => {
-      introVideo.pause();
-      introVideo.currentTime = 0;
-      introVideo.classList.add("hidden");
-      content.classList.remove("hidden");
-      document.body.style.overflow = "visible"; // Allow scrolling
-    });
+  // Slideshow functionality
+  let slideIndex = 0;
 
-    // Rest of your code
-    let slideIndex = 0;
+  function showSlides() {
+    let i;
+    let slides = document.getElementsByClassName("event-image");
 
-    function showSlides() {
-      let i;
-      let slides = document.getElementsByClassName("event-image");
-
-      for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-      }
-
-      slideIndex++;
-      if (slideIndex > slides.length) {
-        slideIndex = 1;
-      }
-
-      slides[slideIndex - 1].style.display = "block";
-
-      // Change slide every 5 seconds (5000 milliseconds)
-      setTimeout(showSlides, 5000);
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
     }
 
-    // Call the function to show the first slide
-    showSlides();
+    slideIndex++;
+    if (slideIndex > slides.length) {
+      slideIndex = 1;
+    }
+
+    slides[slideIndex - 1].style.display = "block";
+
+    // Change slide every 5 seconds (5000 milliseconds)
+    setTimeout(showSlides, 5000);
+  }
+
+  // Call the function to show the first slide
+  showSlides();
+}
 
